7d3885fe3618f0bda4ce9948cf4f4205cb33700e
PedagogicHadithEbook <pedagogichadithebook@gmail.com>
2023-09-09 20:08:26 +0500

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Translation.css";
+export default function About() {
+  return (
+  <>
+    <div className="container3">
+    <center><h3 > Our Mission</h3></center>
+    <p >Our Mission is to Filling the gap between academia and darsey nazami. <br/> This Pedagogic Hadith Ebook of Shamail-e-Tirmidhi is designed by teachers, Alim and students to enhance the learning activity of students.
+    In order to encourage research and advance a mainstream and widely accepted understanding of the Prophet Muhammad's (saws) sunnah, it is our mission to make accurate, comprehensive, and helpful information about it available to as many people as possible worldwide but not to be used for commercial purposes.</p>
+    <center><h3> Hadith Book</h3></center>
+    <p>Shamail-e-Tirmidhi is a revered Hadith collection by Imam al-Tirmidhi, providing a detailed portrayal of Prophet Muhammad's (peace be upon him) physical appearance and exemplary character, inspiring Muslims globally.</p>
+     <center><h3 >Technologies Used For Development</h3></center>
+     <p>Frontend Interface is designed using React.js <br/> Database is designed using JSON Format</p>
+     <center><h3 >About Developers</h3></center>
+     <p>Pedagogic Hadith Ebook of Shamail-e-Tirmidhi is developled by: <br/> Students of Fatima Jinnah Women University, Rawalpindi <br/> Department of Computer Science</p>
+     <center><h3>Acknowledgement</h3></center>
+     <p>This is a non commercial app for educational purposes specifically adapted for wifaq ul madaris curriculum. Text has been prepared in collaboration with experts and Hamna center, where they provided the knowledge base and verified the correctness.</p>
+     <p>We extend our sincerest gratitude to Sunnah.com for providing the authoritative references used in our Pedagogic Hadith Ebook of Shamail-e-Tirmidhi, enabling users to explore the profound teachings of Hadith with authenticity and clarity.</p>
+  </div>
+  </>
+
+  )
+}
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Translation.css";
+
+export default function Home() {
+
+  return (
+    <div className="home">
+      <img src={require('./Home2.PNG')} alt="Pedagogic Hadith Ebook of Shamail-e-Tirmidhi" />
+    </div>
+  );
+}
